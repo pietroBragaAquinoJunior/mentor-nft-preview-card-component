8b1aa8958916f985a7bdd11b254fda2ef1d67678
Pietro Aquino <pietro.aquino@tcema.tc.br>
2024-10-17 09:38:12 -0300

    finalizado

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="css">
+#app{
+  font-family: 'Outfit';
+  font-size: 18px;
+  font-weight: 400;
+  color: hsl(0,0%,100%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-[100vh] bg-[hsl(217,54%,11%)] flex justify-center items-center">
+    <div class="bg-[hsl(216,50%,16%)] w-[330px] h-[540px] rounded-xl p-6 flex flex-col items-start md:w-[400px] md:h-[620px]">
+      <img src="/images/image-equilibrium.jpg" alt="Image Equilibrium" class="rounded-md mb-5">
+      <h1 class="text-[22px] font-[600] mb-3">
+        Equilibrium #3429
+      </h1>
+      <p class="text-[16px] text-[hsl(215,51%,70%)] font-[300] mb-4">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+      <div class="flex justify-between w-full mb-4">
+        <div class="flex w-[90px] items-center justify-between">
+          <img class="w-[12px] h-[17px]" src="/images/icon-ethereum.svg" alt="Icon Ethereum">
+          <p class="text-[hsl(178,100%,50%)] text-[16px]">
+            0.041 ETH
+          </p>
+        </div>
+        <div class="flex w-[90px] items-center justify-between">
+          <img class="w-[15px] h-[15px]" src="/images/icon-clock.svg" alt="Icon Ethereum">
+          <p class="text-[hsl(215,51%,70%)] text-[16px]">
+            3 days left
+          </p>
+        </div>
+      </div>
+      <div class="w-full border-t-[1px] border-[hsl(215,32%,27%)]" />
+      <div class="flex items-center mt-4">
+        <img class="h-[32px] w-[32px] border-solid border-[1px] rounded-full border-[hsl(0,0%,100%)] mr-4" src="/images/image-avatar.png" alt="Image Avatar">
+        <p class="text-[16px]">
+          <span class="text-[hsl(215,51%,70%)]">Creation of</span> Jules Wyvern
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <!-- <body>
+
+  </body> -->
+</template>
